c2517d46efa541d48c9c897fd4fe057c14d25ca0
techieshruti <shrutichandra721@gmail.com>
2025-07-27 14:05:05 +0530

    added startscreen

--- app.js
+++ app.js
@@ -12,13 +12,40 @@ container.style.flexDirection = "column";
 // headings
 const heading1 = document.createElement("h1");
 container.appendChild(heading1);
-heading1.textContent = "TIC-TAC-TOE";
-heading1.style.margin = "3rem";
+heading1.textContent = "Welcome to TIC-TAC-TOE";
+heading1.style.marginTop = "2rem";
 heading1.style.fontSize = "4rem";
+
+// =============== start screen ============
+// =========================================
+ 
+const startscreen=document.createElement("div");
+container.appendChild(startscreen);
+
+// paragraph
+const para = document.createElement("p");
+startscreen.appendChild(para);
+para.textContent=" Tic-Tac-Toe is a classic 2-player strategy game where players take turns placing their symbol (X or O) on a 3x3 grid. The goal is to be the first to get three of your symbols in a row — horizontally, vertically, or diagonally.";
+para.style.fontSize="2rem"
+para.style.margin="2rem"
+
+const heading2=document.createElement("h2");
+startscreen.appendChild(heading2);
+// 
+const ul =document.createElement("ul");
+startscreen.appendChild(ul);
+const li=document.createElement("li");
+startscreen.appendChild(li);
+li.style.margin="3rem"
+li.style.fontSize="1.5rem";
+li.textContent="The game is for 2 players.";
+
+// =============== Game screen =============
+// =========================================
 
 // game grid
 const gridBox = document.createElement("div");
-container.appendChild(gridBox);
+// container.appendChild(gridBox);
 gridBox.style.display = "grid";
 gridBox.style.gridTemplateColumns = "1fr 1fr 1fr";
 gridBox.style.gridTemplateRows = "1fr 1fr 1fr";
@@ -43,27 +70,41 @@ for (let i = 0; i < 9; i++) {
   cell.style.color="#fff";
   cell.style.boxShadow = "2px 2px 10px #943a39ff";
 
-  //
   // icons
   const circle = document.createElement("i");
   cell.appendChild(circle);
+  circle.id=circle;
   circle.classList.add("fa-solid","fa-o");
   circle.style.display="none";
+
   const cross = document.createElement("i");
   cell.appendChild(cross);
+  cross.id=cross;
   cross.classList.add("fa-solid","fa-x");
+  cross.style.display="none";
 
+// MouseOver Event
   cell.addEventListener("mouseover", ()=>{
     cell.style.backgroundColor="#c39531ff";
     cell.style.boxShadow="2px 2px 10px #9a7527ff"
     cell.style.transition="0.5s ease-in";
     cell.style.color="black";
   });
+// MouseOut Event
   cell.addEventListener("mouseout", ()=>{
     cell.style.backgroundColor="#851e1cff";
     cell.style.transition="0.5s ease-in"
   cell.style.boxShadow = "2px 2px 10px #943a39ff";
-    cell.style.color="#fff";
-  
+    cell.style.color="#fff";  
+});
+
+let choice=circle.id;
+// MouseClick Event
+cell.addEventListener("click", ()=>{
+    if(choice === circle.id){
+        circle.style.display="block";
+    } else{
+        cross.style.display="block";
+    }
 });
 }
